19f6224b74ea29e623649c11e1dbb40b856f8a38
IDDna <romanov1371@gmail.com>
2022-07-25 10:42:32 +0300

    Implement animated removing books

---
+++ script.js
@@ -0,0 +1,7 @@
+const book = document.querySelector('.book');
+const removeBtn = document.querySelector('.book__remove');
+
+removeBtn.addEventListener('click', () => {
+   book.style.transform = 'scale(0)';
+   setTimeout(() => book.style.display = 'none', 300)
+})
