d765a46e375365a9b8bc98cfc509bf85775b23e4
IDDna <romanov1371@gmail.com>
2022-07-25 17:02:12 +0300

    Allow to add books and put them in array

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const book = document.querySelectorAll('.book');
+const statusBtns = document.querySelectorAll('.status-btn');
 const removeBtn = document.querySelectorAll('.remove-btn');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = popup.querySelector('.remove-btn');
-const statusBtns = document.querySelectorAll('.status-btn');
+const form = document.querySelector('form')
+const titleInput = document.getElementById('name');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+const statusBtn = popup.querySelector('.status-btn');
+const submitBtn = popup.querySelector('.popup-submit');
 
 let btnStatus = {
    'Not Read': 'not-read',
@@ -10,18 +16,32 @@ let btnStatus = {
    'Read': 'read'
 };
 
-removeBtn.forEach(button => {
-   button.addEventListener('click', () => {
-      book.style.transform = 'scale(0)';
-      setTimeout(() => book.style.display = 'none', 300);
-   });
+let myLibrary = [];
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   const bookTitle = titleInput.value;
+   const bookAuthor = authorInput.value;
+   const bookPages = pagesInput.value;
+   const bookStatus = statusBtn.getAttribute('data-status');
+   const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+   addBookToLibrary(newBook);
 });
 
-function togglePopup() {
-   popup.classList.toggle('active')
+function Book(title, author, pages, status) {
+   this.title = title;
+   this.author = author;
+   this.numOfPages = pages;
+   this.status = status;
 }
 
-popupCloseBtn.addEventListener('click', () => togglePopup());
+function addBookToLibrary(book) {
+   myLibrary.push(book);
+}
+
+function togglePopup() {
+   popup.classList.toggle('active');
+}
 
 statusBtns.forEach(btn => {
    btn.addEventListener('click', () => {
