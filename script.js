d9b525d323bb7b138292fe3a162aede8dca2956c
IDDna <romanov1371@gmail.com>
2022-07-25 14:19:30 +0300

    Show and close popup with buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const book = document.querySelector('.book');
-const removeBtn = document.querySelector('.remove-btn');
+const book = document.querySelectorAll('.book');
+const removeBtn = document.querySelectorAll('.remove-btn');
 const popup = document.querySelector('.popup');
+const popupContent = document.querySelector('.popup__content');
+const popupCloseBtn = popup.querySelector('.remove-btn')
 const popupStatusBtn = popup.querySelector('.status-btn');
 
 let btnStatus = {
@@ -10,13 +12,21 @@ let btnStatus = {
 };
 let step = 1;
 
-removeBtn.addEventListener('click', () => {
-   book.style.transform = 'scale(0)';
-   setTimeout(() => book.style.display = 'none', 300);
-})
+removeBtn.forEach(button => {
+   button.addEventListener('click', () => {
+      book.style.transform = 'scale(0)';
+      setTimeout(() => book.style.display = 'none', 300);
+   });
+});
+
+function togglePopup() {
+   popup.classList.toggle('active')
+}
+
+popupCloseBtn.addEventListener('click', () => togglePopup());
 
 popupStatusBtn.addEventListener('click', () => {
    popupStatusBtn.setAttribute('data-status', btnStatus[Object.keys(btnStatus)[step]])
    popupStatusBtn.textContent = Object.keys(btnStatus)[step++];
    if (step === 3) step = 0;
-})
+});
